ebc5473c5275562c85312250557b2604d7ff0351
baskwalla <baskwalla@gmail.com>
2012-12-13 15:52:08 -0600

    updated to use and create the correct css file

--- inuit.css/watch.sh
+++ inuit.css/watch.sh
@@ -7,6 +7,6 @@
 # No minification
 #sass --watch your-project.scss:your-project.css --style expanded
 
-sass --watch your-project.scss:your-project.min.css --style compressed
+sass --watch style.scss:style.css --style compressed
 
 exit 0

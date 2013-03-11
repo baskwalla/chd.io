fd0dbf206a5a1b01fe8898ed3495b483c33ed45a
baskwalla <baskwalla@gmail.com>
2013-03-10 21:43:24 -0500

    added Typekit embed code

--- kirbycms/site/snippets/footer.php
+++ kirbycms/site/snippets/footer.php
@@ -1,6 +1,9 @@
   <footer>
     <?php echo kirbytext($site->copyright()) ?>
   </footer>
+  
+  <script type="text/javascript" src="//use.typekit.net/fus0cvg.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 </body>
 
